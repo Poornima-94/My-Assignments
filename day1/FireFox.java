5c8a91a07621d883a0dd54ab43178b3e3ec73eb6
Poornima-94 <168276787+Poornima-94@users.noreply.github.com>
2024-04-27 17:53:48 +0530

    Add files via upload
    day1

---
+++ day1/FireFox.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class FireFox {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int releaseYear=1998;
+		String browserName="firefox";
+		float browserVersion=100.2f;
+		boolean isVisible=true;
+		char browserLogo='f';
+		
+		System.out.println(browserVersion);
+		System.out.println(releaseYear);
+		System.out.println(browserName);
+		System.out.println(isVisible);
+		System.out.println(browserLogo);
+	}
+
+}
---
+++ day1/HelloTestLeaf.java
@@ -0,0 +1,21 @@
+package week1.day1;
+
+public class HelloTestLeaf {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Testleaf");
+	int releaseYear=1998;
+	String browserName="firefox";
+	float browserVersion=100.2f;
+	boolean isVisible=true;
+	char browserLogo='f';
+	
+	System.out.println(browserVersion);
+	System.out.println(releaseYear);
+	System.out.println(browserName);
+	System.out.println(isVisible);
+	System.out.println(browserLogo);
+	
+		
+	}
+}
