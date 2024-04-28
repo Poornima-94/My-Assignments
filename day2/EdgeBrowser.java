1034b9d276393c126c9731deed3e9c7729661792
Poornima-94 <168276787+Poornima-94@users.noreply.github.com>
2024-04-28 17:19:57 +0530

    day2

---
+++ day2/EdgeBrowser.java
@@ -0,0 +1,13 @@
+package week2.day2;
+
+public class EdgeBrowser {
+	
+
+	public static void main(String[] args) {
+		Browser e=new Browser();
+e.launchBrowser("Chrome");
+		e.loadUrl();
+	
+		
+	}
+}
