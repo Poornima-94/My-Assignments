2cd8fcddb2d10f72415d43b72166192ad97bc448
Poornima-94 <168276787+Poornima-94@users.noreply.github.com>
2024-05-01 22:43:16 +0530

    Add files via upload
    Week1

---
+++ assignments/FindOddNumberHomeAssignment.java
@@ -0,0 +1,20 @@
+package home.assignments;
+
+public class FindOddNumberHomeAssignment {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int i;
+for(i=1;i<=10;i++)  //i=2;1<=10--True then 2+1=3
+{
+	
+	if (i%2!=0)            //3%2!=0--True print 3
+	{
+		System.out.print("Odd Number from 1 to 10 is:");
+		System.out.println(i);
+				
+	}
+}
+	}
+
+}
