cb952f1a9ce8f37ab868f67642a9cdd98e097d22
Poornima-94 <168276787+Poornima-94@users.noreply.github.com>
2024-05-23 19:46:10 +0530

    Week3Assignments

---
+++ Elements.java
@@ -0,0 +1,24 @@
+package week3.homeassignments;
+
+public class Elements extends Button{
+
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Elements ele=new Elements();
+		ele.submit();
+		ele.click();
+		ele.setText();
+		
+		RadioButton rBt=new RadioButton();
+		rBt.selectRadioButton();
+		
+		CheckBoxButton chkBt=new CheckBoxButton();
+		chkBt.selectRadioButton();
+		
+		
+		
+		
+	}
+
+}
